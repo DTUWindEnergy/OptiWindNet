c2ea60bc81086951486ae22f8680101be2168a1a
Mauricio Souza de Alencar <mdealencar@gmx.com>
2025-07-09 15:17:20 +0200

    doc: improve solver_factory()'s docstring

--- optiwindnet/MILP/__init__.py
+++ optiwindnet/MILP/__init__.py
@@ -4,10 +4,23 @@
 import logging
 from .core import ModelOptions
 
+__all__ = ('ModelOptions', 'solver_factory')
+
 lggr = logging.getLogger(__name__)
 error = lggr.error
 
 def solver_factory(solver_name: str):
+    '''Create a Solver object tied to the specified external MILP solver.
+
+    Note that the only solver that is a dependency of OptiWindNet is 'ortools'.
+    Check OptiWindNet's documentation on how to install optional solvers.
+    
+    Args:
+      solver_name: one of 'ortools', 'cplex', 'gurobi', 'cbc', 'scip', 'highs'.
+
+    Returns:
+      Solver instance that can produce solutions for the cable routing problem.
+    '''
     match solver_name:
         case 'ortools':
             from .ortools import SolverORTools
