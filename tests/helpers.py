5079ba52d8ab1b85c9f7dd1ac14009d11a47c3f3
AmirArasteh <amirarasteh1990@gmail.com>
2025-08-13 13:55:26 +0200

    ignore all of fun_fingerprint for graph assertions

--- tests/helpers.py
+++ tests/helpers.py
@@ -21,8 +21,7 @@ def assert_graph_equal(G1, G2, ignored_graph_keys=None):
         )
 
     for G in (G1, G2):
-        G.graph.get('method_options', {}).get('fun_fingerprint', {}).pop('funfile', None)
-        G.graph.get('method_options', {}).get('fun_fingerprint', {}).pop('funhash', None)
+        G.graph.get('method_options', {}).pop('fun_fingerprint', None)
         G.graph.get('solver_details', {}).pop('strategy', None)
 
     keys1 = set(G1.graph.keys()) - ignored_graph_keys
