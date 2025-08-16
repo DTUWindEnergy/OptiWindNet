DTUWindEnergy/OptiWindNet

commit 6f82aec3b74a63e8058c6cc7e3990d1106845360

fix: replace wfn.G.graph['solution_info'] with wfn.solution_info()

optiwindnet/api.py
@@ -370,6 +370,12 @@ class WindFarmNetwork:
         terse_links = self.terse_links()
         return terse_links
 
+    def solution_info(self):
+        return {
+            k: self.G.graph[k]
+            for k in ('runtime', 'bound', 'objective', 'relgap', 'termination')
+        }
+
 
 class Router(ABC):
     def __init__(self, **kwargs):
@@ -574,16 +580,7 @@ class MILPRouter(Router):
             warmstart=S_warm,
         )
 
-        solution_info = solver.solve(
-            time_limit=self.time_limit,
-            mip_gap=self.mip_gap,
-            options=self.solver_options,
-            verbose=verbose,
-        )
-
         S, G = solver.get_solution()
-
-        G.graph['solution_info'] = solution_info
 
         assign_cables(G, cables)
 

docs/notebooks/a09_large_windfarm_complex_geometry.ipynb
@@ -1331,7 +1331,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b03_MILPRouter_ortools_example.ipynb
@@ -726,7 +726,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b04_MILPRouter_gurobi_example.ipynb
@@ -441,7 +441,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b05_MILPRouter_cplex_example.ipynb
@@ -258,7 +258,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b06_MILPRouter_highs_example.ipynb
@@ -396,7 +396,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b07_MILPRouter_scip_example.ipynb
@@ -458,7 +458,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

docs/notebooks/b08_MILPRouter_cbc_example.ipynb
@@ -427,7 +427,7 @@
     }
    ],
    "source": [
-    "wfn.graph['solution_info']"
+    "wfn.solution_info()"
    ]
   },
   {

commit d16fe6f5a191fe6ccf05eb528af12846ea431048

fix: regression from 6f82aec3

optiwindnet/api.py
@@ -580,6 +580,13 @@ class MILPRouter(Router):
             warmstart=S_warm,
         )
 
+        solver.solve(
+            time_limit=self.time_limit,
+            mip_gap=self.mip_gap,
+            options=self.solver_options,
+            verbose=verbose,
+        )
+
         S, G = solver.get_solution()
 
         assign_cables(G, cables)