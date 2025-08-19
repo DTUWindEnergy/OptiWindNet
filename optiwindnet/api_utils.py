b4380b7491b0434fd10d26673d85c8b3f13a8b51
AmirArasteh <amirarasteh1990@gmail.com>
2025-08-19 22:49:06 +0200

    clean up imprts in api_utils

--- optiwindnet/api_utils.py
+++ optiwindnet/api_utils.py
@@ -1,16 +1,12 @@
 import logging
 import math
-from itertools import pairwise
 from typing import Sequence
-
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Polygon as MplPolygon
 from matplotlib.path import Path
 from shapely.geometry import MultiPolygon, Polygon
 from shapely.validation import explain_validity
-
-from .importer import L_from_site
 
 logger = logging.getLogger(__name__)
 warning, info = logger.warning, logger.info
