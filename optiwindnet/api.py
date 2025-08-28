72fc93731fc7019d9dc8096c3e39c1b2f488c01e
AmirArasteh <amirarasteh1990@gmail.com>
2025-08-28 18:00:39 +0200

    use location-geometry for L

--- optiwindnet/api.py
+++ optiwindnet/api.py
@@ -407,7 +407,7 @@ class WindFarmNetwork:
         return gplot(self.G, *args, **kwargs)
 
     def plot_location(self, **kwargs):
-        """Plot the original location graph."""
+        """Plot the original location geometry."""
         return gplot(self.L, **kwargs)
 
     def plot_available_links(self, **kwargs):
