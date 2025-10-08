5d0497bfbc773786084fa9da74ab967498cbcc2e
AmirArasteh <amirarasteh1990@gmail.com>
2025-10-08 15:20:56 +0200

    ignore 'relgap' when graph assertion

--- tests/helpers.py
+++ tests/helpers.py
@@ -75,6 +75,7 @@ def assert_graph_equal(
     # --- prepare ignored paths --------------------------------------------------
     default_ignored = {
         "bound",
+        "relgap",
         "solver_details",
         "method_options.fun_fingerprint.funfile",
         "method_options.fun_fingerprint.funhash",
