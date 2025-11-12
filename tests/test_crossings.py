c4050bb800ef693c5191bceecc7f13af1bd778d5
AmirArasteh <amirarasteh1990@gmail.com>
2025-11-13 00:56:01 +0100

    test crossings with detours

--- tests/test_crossings.py
+++ tests/test_crossings.py
@@ -10,21 +10,33 @@ def test_get_interferences_list():
     VertexC = np.array(G.graph["VertexC"])
     Edge = np.array(list(G.edges()))
 
-
     with pytest.raises(IndexError):
         get_interferences_list(Edge=Edge, VertexC=VertexC)
 
-
-    fnT = wfn.G.graph['fnT']
+    fnT = G.graph['fnT']
 
     crossings_0 = get_interferences_list(Edge=Edge, VertexC=VertexC, fnT=fnT)
     assert crossings_0 == []
-
 
     Edge[1] = (-1, 11)
     crossings_1 = get_interferences_list(Edge=Edge, VertexC=VertexC, fnT=fnT)
     expected = [((np.int64(0), np.int64(12), np.int64(-1), np.int64(11)), None)]
     assert crossings_1 == expected
+
+    # with detours
+    wfn2 = tiny_wfn(cables=1)
+    G2 = wfn2.G
+    VertexC2 = np.array(G2.graph["VertexC"])
+    Edge2 = np.array(list(G2.edges()))
+    fnT2 = G2.graph['fnT']
+
+    crossings_2 = get_interferences_list(Edge=Edge2, VertexC=VertexC2, fnT=fnT2)
+    assert crossings_2 == []
+
+    crossings_3 = get_interferences_list(Edge=Edge2, VertexC=VertexC2, fnT=fnT)
+    expected = [((np.int64(-1), np.int64(13), np.int64(1), np.int64(12)), None)]
+    assert crossings_3 == expected
+
 
 def test_validate_routeset():
     wfn = tiny_wfn()
@@ -41,3 +53,9 @@ def test_validate_routeset():
     G.remove_edge(0, 12)
     with pytest.raises(AssertionError):
         validate_routeset(G)
+
+    # with detours
+    wfn2 = tiny_wfn(cables=1)
+    G2 = wfn2.G
+    with pytest.raises(KeyError):
+        validate_routeset(G2)
