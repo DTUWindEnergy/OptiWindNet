48500d9aa0006b3b0f8e0725a320cf4ebff54961
Mauricio Souza de Alencar <mdealencar@gmx.com>
2025-12-03 15:11:07 +0100

    test: make test_MILP_solvers spawn a new process for each solver (workaround for DLL hell)

--- tests/test_MILP.py
+++ tests/test_MILP.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import multiprocessing
 
 from optiwindnet.synthetic import toyfarm
 from optiwindnet.mesh import make_planar_embedding
@@ -10,6 +11,18 @@ from optiwindnet.interarraylib import terse_links_from_S
 # topology in terse links for toy_farm at capacity=5
 _terse_toy_farm_5 = np.array([2, -1, 1, 2, -1, -1, 3, 4, -1, 5, 8, 8])
 _CAPACITY = 5
+_RUNTIME = 1
+_GAP = 0.001
+
+
+# Loading solvers ortools and scip within the same python instance causes DLL hell
+# (ortools contains a SCIP library). Typical usage of OWN is with a single solver.
+# Use a workaround for tests: spawn a new python process for each solver.
+def _worker_MILP_solver(P, A, solver_name) -> None | tuple:
+    solver = solver_factory(solver_name)
+    solver.set_problem(P, A, capacity=_CAPACITY, model_options=ModelOptions())
+    solution_info = solver.solve(time_limit=_RUNTIME, mip_gap=_GAP)
+    return solution_info, solver.get_solution()
 
 
 @pytest.fixture(scope='module')
@@ -24,17 +37,15 @@ def P_A_toy():
     ['ortools', 'gurobi', 'cplex', 'highs', 'scip', 'cbc'],
 )
 def test_MILP_solvers(P_A_toy, solver_name):
+    ctx = multiprocessing.get_context('spawn')
+
     try:
-        solver = solver_factory(solver_name)
+        with ctx.Pool(1) as pool:
+            solution_info, (S, G) = pool.apply(
+                _worker_MILP_solver, args=(*P_A_toy, solver_name)
+            )
     except (FileNotFoundError, ModuleNotFoundError):
         pytest.skip(f'{solver_name} not available')
 
-    solver.set_problem(
-        *P_A_toy,
-        capacity=_CAPACITY,
-        model_options=ModelOptions(),
-    )
-    solution_info = solver.solve(time_limit=1, mip_gap=0.001)
     assert solution_info.termination.lower() == 'optimal'
-    S, _ = solver.get_solution()
     assert (terse_links_from_S(S) == _terse_toy_farm_5).all()
