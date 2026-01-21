ae22591af5385f326782467520a69b1367992f55
Mauricio Souza de Alencr <mdealencar@gmx.com>
2026-01-21 13:31:10 +0100

    chore: remove deprecated class NodeTagger

--- optiwindnet/utils.py
+++ optiwindnet/utils.py
@@ -17,40 +17,6 @@ def namedtuplify(namedtuple_typename='', **kwargs):
     return NamedTuplified(**kwargs)
 
 
-class NodeTagger:
-    # 50 digits, 'I' and 'l' were dropped
-    alphabet = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ'
-    value = {c: i for i, c in enumerate(alphabet)}
-
-    def __getattr__(self, b50):
-        dec = 0
-        digit_value = 1
-        if b50[0] < 'α':
-            for digit in b50[::-1]:
-                dec += self.value[digit] * digit_value
-                digit_value *= 50
-            return dec
-        else:
-            # for greek letters, only single digit is implemented
-            return ord('α') - ord(b50[0]) - 1
-
-    def __getitem__(self, dec):
-        if dec is None:
-            return '∅'
-        elif isinstance(dec, str):
-            return dec
-        b50 = []
-        if dec >= 0:
-            while True:
-                dec, digit = divmod(dec, 50)
-                b50.append(self.alphabet[digit])
-                if dec == 0:
-                    break
-            return ''.join(b50[::-1])
-        else:
-            return chr(ord('α') + (abs(dec) - 1) % 25)
-
-
 class Alerter:
     def __init__(self, where, varname):
         self.where = where
