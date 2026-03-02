38bbff6e8e10dcaa18809e36ac4945b224eb05e2
Mauricio Souza de Alencar <mdealencar@gmx.com>
2026-03-02 18:21:53 +0100

    feat: drop py.io by using a contextlib-compatible new version of hybgensea

--- optiwindnet/baselines/_core_hgs.py
+++ optiwindnet/baselines/_core_hgs.py
@@ -4,11 +4,12 @@
 # [[2012.10384] Hybrid Genetic Search for the CVRP: Open-Source Implementation
 #  and SWAP\* Neighborhood]
 # (https://arxiv.org/abs/2012.10384)
+import io
+from contextlib import redirect_stdout
 from dataclasses import asdict
 
 import hybgensea as hgs
 import numpy as np
-from py.io import StdCaptureFD
 
 
 def _solution_time(log, objective) -> float:
@@ -47,8 +48,10 @@ def do_hgs(W, coordinates, vehicles, capacity, solver_options):
     ap = hgs.AlgorithmParameters(**solver_options)
     hgs_solver = hgs.Solver(parameters=ap, verbose=True)
 
-    result, log, _ = StdCaptureFD.call(hgs_solver.solve_cvrp, data, rounding=False)
-
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        result = hgs_solver.solve_cvrp(data, rounding=False)
+    log = buffer.getvalue()
     solution_time = _solution_time(log, result.cost)
 
     return (result.routes, result.time, solution_time, result.cost, log, asdict(ap))
